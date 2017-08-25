4944202699db28555815aaa1c305c5da211ee377
Bryan Dela Cruz <bdshi13@gmail.com>
2017-08-25 18:46:24 +0800

    Finished now watching page

--- src/pages/channel-prev/channel-prev.ts
+++ src/pages/channel-prev/channel-prev.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ChannelPrevPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-channel-prev',
   templateUrl: 'channel-prev.html',
