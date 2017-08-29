c7cbb39bfdbb0d86259532b4917fc2e52b9abf1f
Brighttank <brighttank@DEV_IOSTEST_W.brighttank.admin>
2017-08-29 15:29:58 +0800

    User page Done minor view fixes

--- src/pages/channel-prev/channel-prev.ts
+++ src/pages/channel-prev/channel-prev.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
+import { PopoverPage } from "../../app/popover";
 
 /**
  * Generated class for the ChannelPrevPage page.
@@ -15,11 +16,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChannelPrevPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public popoverCtrl: PopoverController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChannelPrevPage');
+  }
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
   }
 
 }
