8a532d94a31315eff505002c2450f508b5c1b76d
Rico Raven <ricoraven28@gmail.com>
2017-09-25 13:22:33 +0800

    Upload [ongoing]

--- src/pages/upload-video/upload-video.ts
+++ src/pages/upload-video/upload-video.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 /**
  * Generated class for the UploadVideoPage page.
  *
@@ -24,49 +26,77 @@ export class UploadVideoPage {
   level = '';
   targetMarketLoc = [];
   allowComment = '';
-  allowSharing = ''; 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  allowSharing = '';
+  vidValue;
+  fileTransfer: FileTransferObject = this.transfer.create();
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private transfer: FileTransfer,
+    private file: File,
+    private camera: Camera) {
     this.categories = [
-      {title: 'Entertainment',value: 'Entertainment'},
-      {title: 'Messages',value: 'Messages'},
-      {title: 'Products',value: 'Products'},
-      {title: 'Highlights',value: 'Highlights'},
-      {title: 'Bissiness Methods',value: 'Bissiness Methods'},
-      {title: 'Film and Animation',value: 'Film and Animation'},
-      {title: 'Testimonials',value: 'Testimonials'},
-      {title: 'Personal Development',value: 'Personal Development'},
-      {title: 'Annoucements',value: 'Annoucements'},
-      {title: 'Multilingual',value: 'Multilingual'},
-      {title: 'Causes and Non-profits',value: 'Causes and Non-profits'}
+      { title: 'Entertainment', value: 'Entertainment' },
+      { title: 'Messages', value: 'Messages' },
+      { title: 'Products', value: 'Products' },
+      { title: 'Highlights', value: 'Highlights' },
+      { title: 'Bissiness Methods', value: 'Bissiness Methods' },
+      { title: 'Film and Animation', value: 'Film and Animation' },
+      { title: 'Testimonials', value: 'Testimonials' },
+      { title: 'Personal Development', value: 'Personal Development' },
+      { title: 'Annoucements', value: 'Annoucements' },
+      { title: 'Multilingual', value: 'Multilingual' },
+      { title: 'Causes and Non-profits', value: 'Causes and Non-profits' }
     ]
     this.levels = [
-      {title: 'General',value: 'General'},
-      {title: 'Beginner',value: 'Beginner'},
-      {title: 'Intermediate',value: 'Intermediate'},
-      {title: 'Advanced',value: 'Advanced'},
-      {title: 'Platinum',value: 'Platinum'},
+      { title: 'General', value: 'General' },
+      { title: 'Beginner', value: 'Beginner' },
+      { title: 'Intermediate', value: 'Intermediate' },
+      { title: 'Advanced', value: 'Advanced' },
+      { title: 'Platinum', value: 'Platinum' },
     ]
-    this.locs= [
-      {title: 'Africa'},
-      {title: 'Australia'},
-      {title: 'Central Asia'},
-      {title: 'East Asia'},
-      {title: 'Europe'},
-      {title: 'Middle East'},
-      {title: 'North America'},
-      {title: 'Pacific Islands'},
-      {title: 'South America'},
-      {title: 'South Asia'},
-      {title: 'Southeast Asia'}
+    this.locs = [
+      { title: 'Africa' },
+      { title: 'Australia' },
+      { title: 'Central Asia' },
+      { title: 'East Asia' },
+      { title: 'Europe' },
+      { title: 'Middle East' },
+      { title: 'North America' },
+      { title: 'Pacific Islands' },
+      { title: 'South America' },
+      { title: 'South Asia' },
+      { title: 'Southeast Asia' }
     ]
   }
 
   ionViewDidLoad() {
     //open file picker here
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.VIDEO
+    }
+    this.camera.getPicture(options).then(ii=>{
+      this.vidValue = ii;
+    });
   }
 
-  uploadVid(){
+  uploadVid() {
+
     console.log("Upload Video Clicked!")
+    let options: FileUploadOptions = {
+      fileKey: 'file',
+      fileName: 'name.jpg',
+      headers: {}
+    }
+
+    this.fileTransfer.upload('<file path>', '<api endpoint>', options)
+      .then((data) => {
+        // success
+      }, (err) => {
+        // error
+      })
   }
 
 }
